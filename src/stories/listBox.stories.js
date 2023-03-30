93dae7d3e6f6e9e89bef3159605cdf5c749d6a62
김동현 <koz8615@kakao.com>
2023-03-30 20:44:48 +0900

    feat: 작업물

---
+++ src/stories/listBox.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ListBox from "../components/common(공통컴포넌트)/ListBox";
+
+export default {
+  title: "listBox",
+  component: ListBox,
+};
+
+export const Template = (args) => <ListBox {...args} />;
